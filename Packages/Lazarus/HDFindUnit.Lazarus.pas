b28f2fedb23983adae6369b753ab5a7b15888f65
Rtrevisan20 <harddevinfo@gmail.com>
2026-09-25 14:01:37 -0300

    :bug: fix: Chamar RegisterHDFindUnit na Register da unit raiz
    Ao consolidar o pacote unico, a procedure Register da unit raiz ficou
    vazia e o atalho Ctrl+Shift+A nao era registrado na IDE. Agora o
    Register chama RegisterHDFindUnit, que registra o comando IDE e o
    menu em itmCustomTools.

--- Packages/Lazarus/HDFindUnit.Lazarus.pas
+++ Packages/Lazarus/HDFindUnit.Lazarus.pas
@@ -8,25 +8,26 @@ unit HDFindUnit.Lazarus;
 interface
 
 uses
-  DelphiAST.Consts, DelphiAST.Classes, SimpleParser.Types, SimpleParser.Lexer.Types,
-  SimpleParser.Lexer, SimpleParser, DelphiAST, DelphiAST.Writer, StringBuilderUnit,
-  Log4Pascal, SVG, SVGColor, SVGCommon, SVGPaint, SVGParse, SVGPath,
-  SVGProperties, SVGStyle, SVGTypes, SVGXML, Painter, PainterGdiPlus, PainterLCL,
-  GDIPAPI, GDIPOBJ, GDIPOBJ2, GDIPKerning, GDIPPathText, DirectDraw, RFUNetEncoding,
-  HDFindUnit.Model.Header, HDFindUnit.Model.DelphiReservedWords,
-  HDFindUnit.Model.StringPositionList, HDFindUnit.Model.Settings,
-  HDFindUnit.Model.PasParser, HDFindUnit.Model.FileCache,
-  HDFindUnit.Model.Interf.SearchStringCache, HDFindUnit.Model.SearchStringCache,
-  HDFindUnit.Model.SearchString, HDFindUnit.Model.DelphiVlcWrapper,
-  HDFindUnit.View.FormMessage, HDFindUnit.Utils,
-  HDFindUnit.Model.Interf.Translation, HDFindUnit.Model.Translation,
-  HDFindUnit.Controller.Interf.EnvironmentController,
-  HDFindUnit.Model.IncluderHandlerInc,
-  HDFindUnit.Model.ResultsImportanceCalculator, HDFindUnit.Model.AutoImport,
-  HDFindUnit.Controller.OTAUtils, HDFindUnit.Controller.EnvironmentController,
-  HDFindUnit.Model.FileEditor, HDFindUnit.Model.Worker,
-  HDFindUnit.View.FormSearch, HDFindUnit.Controller.Lazarus,
-  LazarusPackageIntf;
+  DelphiAST.Consts, DelphiAST.Classes, SimpleParser.Types, 
+  SimpleParser.Lexer.Types, SimpleParser.Lexer, SimpleParser, DelphiAST, 
+  DelphiAST.Writer, StringBuilderUnit, Log4Pascal, SVG, SVGColor, SVGCommon, 
+  SVGPaint, SVGParse, SVGPath, SVGProperties, SVGStyle, SVGTypes, SVGXML, 
+  Painter, PainterGdiPlus, PainterLCL, GDIPAPI, GDIPOBJ, GDIPOBJ2, 
+  GDIPKerning, GDIPPathText, DirectDraw, RFUNetEncoding, 
+  HDFindUnit.Model.Header, HDFindUnit.Model.DelphiReservedWords, 
+  HDFindUnit.Model.StringPositionList, HDFindUnit.Model.Settings, 
+  HDFindUnit.Model.PasParser, HDFindUnit.Model.FileCache, 
+  HDFindUnit.Model.Interf.SearchStringCache, 
+  HDFindUnit.Model.SearchStringCache, HDFindUnit.Model.SearchString, 
+  HDFindUnit.Model.DelphiVlcWrapper, HDFindUnit.View.FormMessage, 
+  HDFindUnit.Utils, HDFindUnit.Model.Interf.Translation, 
+  HDFindUnit.Model.Translation, 
+  HDFindUnit.Controller.Interf.EnvironmentController, 
+  HDFindUnit.Model.IncluderHandlerInc, 
+  HDFindUnit.Model.ResultsImportanceCalculator, HDFindUnit.Model.AutoImport, 
+  HDFindUnit.Controller.OTAUtils, HDFindUnit.Controller.EnvironmentController, 
+  HDFindUnit.Model.FileEditor, HDFindUnit.Model.Worker, 
+  HDFindUnit.View.FormSearch, HDFindUnit.Controller.Lazarus, LazarusPackageIntf;
 
 implementation
 
